99fd8e6c1524c4f7394d4a0aeebd6b7d1fffc7f4
Mladen Ivkovic <mladen.ivkovic@hotmail.com>
2019-10-21 15:56:31 +0200

    spack and compiler fixup

--- scripts/spackinstall.sh
+++ scripts/spackinstall.sh
@@ -7,14 +7,17 @@
 # module purge
 # spack uninstall --all
 
+spack install -y lmod %gcc@7.4.0
+
+for compiler in gcc@7.4.0 gcc@8.3.0 clang@6.0.0-1ubuntu2 ; do
+
 
 #--------------------
 # core dependencies
 #--------------------
 
-# spack install lmod
-# spack install openmpi@3.0.3
-# spack install mpich
+spack install -y openmpi %$compiler
+spack install -y mpich %$compiler
 
 
 #-----------------------
@@ -27,21 +30,23 @@
 for dep in openmpi mpich; do
 
     for pack in parmetis; do
-        spack install $pack ^$dep
+        spack install -y $pack ^$dep %$compiler
     done;
 
     # +openmp: install with openmp too
     # ^$dep needs to be after +openmp, otherwise all other
     # specifications are taken to be for the dependency
-    spack install fftw@3.3.8 +openmp ^$dep
-    spack install fftw@2.1.5 +openmp ^$dep
+    spack install -y fftw@3.3.8 +openmp %$compiler ^$dep
+    spack install -y fftw@2.1.5 +openmp %$compiler ^$dep
 done;
 
 
 # without
 
-# for pack in jemalloc gsl metis; do
-#     spack install $pack
-# done;
+for pack in jemalloc gsl metis; do
+    spack install -y $pack %$compiler
+done;
 
-spack install hdf5 +cxx +fortran +threadsafe
+spack install -y hdf5 +cxx +fortran +threadsafe %$compiler
+
+done;
