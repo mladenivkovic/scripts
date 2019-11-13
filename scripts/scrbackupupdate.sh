0a8ad6bfe23da70aef807c922d5318ae1f55c7dd
Mladen Ivkovic <mladen.ivkovic@hotmail.com>
2019-11-13 17:59:13 +0100

    more elegant way of doing scrbackupupdate

--- scripts/scrbackupupdate.sh
+++ scripts/scrbackupupdate.sh
@@ -23,6 +23,7 @@ if [ $# == 0 ]; then
 
     echo "USING HARDCODED ORIGINAL LINKS FILE."
     echo "IF YOU WANT TO USE A DIFFERENT FILE, SPECIFY IT AS CMD LINE ARG."
+    echo
     SRCFILE=$PJ/online-backup/original_links.txt
     
 elif [ $# == 1 ]; then
@@ -54,25 +55,72 @@ else
 fi
 
 
+
+
+
+
+#============================================================
+# Read in files line by line, and update the scripts
+#============================================================
+
+
 while read line; do
     src=`echo $line | sed 's/:/ /g' | awk '{print $1}'`
     dest=`echo $line | sed 's/:/ /g' | awk '{print $2}'`
 
+    do_check=true
+    # set them to 'false' here because they will be used as a check
+    src_rsync=
+    dest_rsync=
 
-    # do rsync in both directions, such that wherever you modified the file,
-    # it will be updated.
-    # scrbackup --no-log "$src" "$dest"
+    if [ ! -f "$src" ]; then
+        # 'src' file doesn't exist
+        src_rsync="$dest"
+        dest_rsync="$src"
+        do_check=
+    fi
 
-    # use rsync instead of hard/soft links because git breaks links
-    echo "$src" '-->' "$dest"
-    rsync -a --update "$src" "$dest"
 
-    echo "$dest" '-->' "$src"
-    rsync -a --update "$dest" "$src"
+    [ "$do_check" ] && \
+    if [ ! -f "$dest" ]; then
+        src_rsync="$src"
+        dest_rsync="$dest"
+        do_check=
+    fi
 
-    if [ $? -ne 0 ]; then
-        echo 'In case you forgot: I need to have scrbackup in the path so I can run it'
-        exit 1
+    [ "$do_check" ] && \
+    if [ "$src" -nt "$dest" ]; then
+        src_rsync="$src"
+        dest_rsync="$dest"
+    elif [ "$dest" -nt "$src" ]; then
+        src_rsync="$dest"
+        dest_rsync="$src"
+    fi;
+
+
+    # if there is work to do, $src_rsync will be defined
+    if [ "$src_rsync" ]; then
+
+        # use rsync instead of hard/soft links because git breaks links
+        # echo "$src_rsync" '-->' "$dest_rsync"
+        echo "SOURCE:     " "$src_rsync"
+        echo "DESTINATION:" "$dest_rsync"
+        echo
+
+        rsync -a --update "$src_rsync" "$dest_rsync"
+        # update time
+        touch -m -c  "$dest_rsync" "$src_rsync"
+
+        # old version
+        # scrbackup --no-log "$src_rsync" "$dest_rsync"
+        # if [ $? -ne 0 ]; then
+        #     echo 'In case you forgot: I need to have scrbackup in the path so I can run it'
+        #     exit 1
+        # fi
     fi
 
 done < "$SRCFILE"
+
+
+
+echo "Finished updating backed up scripts."
