457d9bfc8020f9efed7b502a668756fcf791d737
Mladen Ivkovic <mladen.ivkovic@hotmail.com>
2020-07-22 20:54:06 +0200

    updated swift quickplot to use swiftsimio

--- scripts/swift/swift-quickplot.py
+++ scripts/swift/swift-quickplot.py
@@ -9,51 +9,46 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import colors as mcolors
 
-import h5py
-import fast_histogram as fh
 import subprocess
 import argparse
 
-from mpl_toolkits.axes_grid1 import make_axes_locatable, axes_size 
+from swiftsimio import load
+from swiftsimio.visualisation.projection import project_gas
 
 
 infile = None
-weight = None
-weighted = False
-ptype = None
 
-weightarraynames = {};
-weightarraynames['sl'] = 'SmoothingLength'
-weightarraynames['mass'] = 'Mass'
-weightarraynames['dens'] = 'Density'
+names = {}; # dict of plotting cases -> array names
+names['mass'] = 'masses'
+names['density'] = 'densities'
+names['h'] = 'smoothing_lengths'
+names['internal energy'] = 'internal_energies'
+names['entropy'] = 'entropies'
 
-outfile = None
+nx_default = 1024
 
-nbinsdefault = 200
-nbins = nbinsdefault
-
-x_to_y_ratio = 1
 
 
 
 # Plot parameters
-params = {'axes.labelsize': 10,
-'axes.titlesize': 10,
-'font.size': 12,
-'legend.fontsize': 12,
-'xtick.labelsize': 10,
-'ytick.labelsize': 10,
-#  'text.usetex': True,
-'figure.subplot.left'    : 0.045,
-'figure.subplot.right'   : 0.99,
-'figure.subplot.bottom'  : 0.05,
-'figure.subplot.top'     : 0.99,
-'figure.subplot.wspace'  : 0.15,
-'figure.subplot.hspace'  : 0.12,
-'lines.markersize' : 6,
-'lines.linewidth' : 3.,
-#  'text.latex.unicode': False
-}
+params = {
+        'axes.labelsize': 10,
+        'axes.titlesize': 12,
+        'font.size': 10,
+        'legend.fontsize': 10,
+        'xtick.labelsize': 10,
+        'ytick.labelsize': 10,
+        'text.usetex': True,
+        'figure.subplot.left'    : 0.02,
+        'figure.subplot.right'   : 0.91,
+        'figure.subplot.bottom'  : 0.08,
+        'figure.subplot.top'     : 0.96,
+        'figure.subplot.wspace'  : 0.15,
+        'figure.subplot.hspace'  : 0.12,
+        'lines.markersize' : 6,
+        'lines.linewidth' : 3.,
+        #  'text.latex.unicode': False
+    }
 plt.rcParams.update(params)
 plt.rc('font',**{'family':'sans-serif','sans-serif':['Times']})
 
@@ -69,168 +64,75 @@ def getargs():
 
     parser = argparse.ArgumentParser(description='''
         A program to quickly plot swift outputs.
-        Will histogram by default number density on xy plane.
-            ''')
+        Will plot mass projection along z axis as default.
+        ''')
 
 
     #  parser.add_help(True)
     parser.add_argument('filename')
     parser.add_argument('--mass', 
-            dest='weight', 
+            dest='to_plot', 
             action='store_const', 
             const='mass', 
-            help='use mass as weight')
+            default='mass',
+            help='project mass')
     parser.add_argument('--sl', 
-            dest='weight', 
+            dest='to_plot', 
             action='store_const', 
             const='h', 
-            help='use smoothing length as weight')
+            default='mass',
+            help='project smoothing length')
     parser.add_argument('--dens', 
-            dest='weight', 
+            dest='to_plot', 
             action='store_const', 
-            const='dens', 
-            help='use particle density as weight')
-    parser.add_argument('--pt',
-            dest='ptype', 
-            action='store',
-            default='PartType0',
-            help='PartType to use. Default=PartType0')
-    parser.add_argument('--dm', 
-            dest='ptype', 
+            const='density', 
+            default='mass',
+            help='project mass density')
+    parser.add_argument('--rho', 
+            dest='to_plot', 
             action='store_const', 
-            const='PartType1', 
-            help='use dark matter particle type (PartType1)')
+            const='density', 
+            default='mass',
+            help='project mass density')
+    parser.add_argument('--u', 
+            dest='to_plot', 
+            action='store_const', 
+            const='internal energy', 
+            default='mass',
+            help='project internal energy')
+    parser.add_argument('--ent', 
+            dest='to_plot', 
+            action='store_const', 
+            const='entropy', 
+            default='mass',
+            help='project entropy')
+    #  parser.add_argument('--pt',
+    #          dest='ptype',
+    #          action='store',
+    #          default='PartType0',
+    #          help='PartType to use. Default=PartType0')
+    #  parser.add_argument('--dm',
+    #          dest='ptype',
+    #          action='store_const',
+    #          const='PartType1',
+    #          help='use dark matter particle type (PartType1)')
     parser.add_argument('--nx', 
             dest='nx', 
             type=int,
             action='store', 
-            default=nbinsdefault,
-            help='How many histogram bins to use.')
+            default=nx_default,
+            help='Image pixel resolution.')
 
     args = parser.parse_args()
 
-    global infile, weight, weighted, ptype, nbins
     infile = args.filename
-    weight = args.weight
-    ptype = args.ptype
-    nbins = args.nx
+    to_plot = args.to_plot
+    nx = args.nx
 
-    if weight is not None:
-        weighted = True
-
-    return
+    return infile, to_plot, nx
 
 
 
-
-#===============================
-def histogram():
-#===============================
-    """
-    Read in data, histogram them
-    """
-
-    global nbins, x_to_y_ratio
-
-    f = h5py.File(infile)
-
-    x = f[ptype]['Coordinates'][:, 0]
-    y = f[ptype]['Coordinates'][:, 1]
-
-    try:
-        xmax = f['Header'].attrs['BoxSize'][0]
-        ymax = f['Header'].attrs['BoxSize'][1]
-    except IndexError:
-        xmax = f['Header'].attrs['BoxSize']
-        ymax = xmax
-
-    x_to_y_ratio = xmax/ymax
-
-    if weighted:
-        w = f[ptype][weightarraynames[weight]][:] 
-    else:
-        w = None
-
-    hist = fh.histogram2d(x, y, range=[[0, xmax], [0, ymax]], bins=[nbins+1, int(nbins/x_to_y_ratio+0.5)+1], weights=w)
-
-    return [hist, xmax, ymax]
-
-
-
-
-
-
-#===============================
-def make_plot(histdata):
-#===============================
-    """
-    plot and save.
-    """
-
-    global outfile
-
-    hist = histdata[0]
-    xmax = histdata[1]
-    ymax = histdata[2]
-
-    fig = plt.figure(figsize=(15, 15/x_to_y_ratio+0.5))
-    ax = fig.add_subplot(111, aspect='equal')
-
-    # shift the x/y values:
-    # hist[0,0] should be plotted at (dx, dy)
-    dx = 0.5*xmax/nbins
-    dy = 0.5*ymax/nbins
-
-    minval = np.abs(hist).min()
-
-    norm = mcolors.Normalize()
-
-    if minval == 0:
-        linthresh = 1e-3
-    else:
-        linthresh =  minval
-
-    im=ax.imshow(hist.T, origin='lower', 
-            extent=(-dx, xmax+dx, -dy, ymax+dy), 
-            cmap='YlGnBu_r', 
-            #  interpolation='None',
-            interpolation='bicubic',
-            norm=mcolors.SymLogNorm(linthresh))
-
-    divider = make_axes_locatable(ax)
-    cax = divider.append_axes("right", size="2%", pad=0.05)
-    fig.colorbar(im, cax=cax)
-
-
-
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    
-    if weighted:
-        ax.set_title('Weighted by '+weight)
-    else:
-        ax.set_title('Number density')
-
-
-    fig.suptitle(infile)
-    plt.tight_layout(rect=(0, 0, 1, 0.97))
-
-
-
-    if infile[-5:] == '.hdf5':
-        outfile = infile.replace('.hdf5', '') 
-    elif infile[-3:] == '.h5':
-        outfile = infile.replace('.h5', '') 
-    if weighted:
-        outfile+=weight
-    if nbins != nbinsdefault:
-        outfile+='_nx='+str(nbins)
-    outfile += '.png'
-
-    plt.savefig(outfile, dpi=200)
-    
-
-    return
 
 
 
@@ -239,10 +141,51 @@ def make_plot(histdata):
 def main():
 #==========================
 
-    getargs()
-    histdata = histogram()
-    make_plot(histdata)
+    infile, to_plot, nx = getargs()
 
+    data = load(infile)
+    meta = data.metadata
+    boxsize = meta.boxsize
+    redshift = meta.redshift
+    time = meta.t.in_units("Gyr")
+
+    mymap = project_gas(data, 
+                resolution=nx,
+                project=names[to_plot], 
+                parallel=True)
+
+
+    fig = plt.figure(figsize=(7, 6))
+    ax = fig.add_subplot(111, aspect='equal')
+
+    im=ax.imshow(mymap.value, 
+            origin='lower', 
+            cmap='YlGnBu_r', 
+            extent = (0, boxsize.value[0], 0, boxsize.value[1]), 
+            norm=mcolors.SymLogNorm(1e-6), 
+        )
+
+    cb = fig.colorbar(im, fraction=0.046, pad=0.01)
+    cb.ax.set_ylabel(to_plot + " [$" + mymap.units.latex_repr + "$]")
+
+    title = r"\verb|{}|".format(infile)
+    title += "; z = {0:.3f}, t= {1:.3e}".format(redshift, time)
+
+
+    ax.set_title(title)
+    ax.set_xlabel("x [{}]".format(boxsize.units))
+    ax.set_ylabel("y [{}]".format(boxsize.units))
+
+
+
+    if infile[-5:] == '.hdf5':
+        outfile = infile.replace('.hdf5', '') 
+    elif infile[-3:] == '.h5':
+        outfile = infile.replace('.h5', '') 
+    outfile += '-{}.png'.format(names[to_plot])
+
+    plt.savefig(outfile, dpi=200)
+    
     subprocess.run(['eog', outfile])
 
 
