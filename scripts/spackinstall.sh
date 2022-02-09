f6c21d23baf7e4853ebe3c998f51bc177cbaf48a
Mladen Ivkvoic <mladen.ivkovic@hotmail.com>
2022-02-09 17:26:35 +0100

    after war with spackinstall

--- scripts/spackinstall.sh
+++ scripts/spackinstall.sh
@@ -11,23 +11,23 @@
 # my native compiler
 native="gcc@9.3.0"
 
-spack install -y lmod %"$native"
+# spack install -y lmod %"$native"
 
 # make modules work
-source ~/.bashrc_modules
+# source ~/.bashrc_modules
 
 # install compilers
 # for compiler in gcc@8.4.0 gcc@9.2.0 gcc@10.2.0; do
-for compiler in gcc@10.2.0; do
-    spack install -y "$compiler" %"$native"
-
-    # make sure spack knows about its compilers
-    module purge
-    compiler_module=`echo "$compiler" | sed 's:@:/:'`
-    module load "$compiler_module"
-    spack compiler add
-    module purge
-done
+# for compiler in gcc@10.2.0 gcc@11.2.0; do
+#     spack install -y "$compiler" %"$native"
+#
+#     # make sure spack knows about its compilers
+#     module purge
+#     compiler_module=`echo "$compiler" | sed 's:@:/:'`
+#     module load "$compiler_module"
+#     spack compiler add
+#     module purge
+# done
 
 
 
@@ -37,79 +37,80 @@ done
 # for compiler in gcc@8.4.0 ; do
 # for compiler in gcc@9.2.0 ; do
 # for compiler in gcc@10.2.0 ; do
-for compiler in "$native" gcc@10.2.0 ; do
+# for compiler in "$native" gcc@10.2.0 gcc@11.2.0; do
+for compiler in gcc@10.2.0; do
 
-module purge
-if [ "$compiler" == "$native" ]; then
-    echo "skipping native compiler module:" $native
-    compiler_module=""
-else
-    compiler_module=`echo "$compiler" | sed 's:@:/:'`
-    module load "$compiler_module"
-fi
-
-
-# clang needs extra flags
-# if [[ $compiler == "clang"* ]]; then
-#     echo got clang
-#     CFLAGS=-fPIC
-#     CXXFLAGS=-fPIC
-#     FFLAGS=-fPIC
-# else
-#     CFLAGS=
-#     CXXFLAGS=
-#     FFLAGS=
-# fi
-
-#--------------------
-# core dependencies
-#--------------------
-
-spack install -y openmpi %"$compiler"
-# spack install -y openmpi cflags="$CFLAGS" cxxflags="$CXXFLAGS" fflags="$FFLAGS" %"$compiler"
-# spack install -y mpich   cflags="$CFLAGS" cxxflags="$CXXFLAGS" fflags="$FFLAGS" %"$compiler"
-# exit
+    module purge
+    if [ "$compiler" == "$native" ]; then
+        echo "skipping native compiler module:" $native
+        compiler_module=""
+    else
+        compiler_module=`echo "$compiler" | sed 's:@:/:'`
+        module load "$compiler_module"
+    fi
 
 
-#-----------------------
-# individual packages
-#-----------------------
+    # clang needs extra flags
+    # if [[ $compiler == "clang"* ]]; then
+    #     echo got clang
+    #     CFLAGS=-fPIC
+    #     CXXFLAGS=-fPIC
+    #     FFLAGS=-fPIC
+    # else
+    #     CFLAGS=
+    #     CXXFLAGS=
+    #     FFLAGS=
+    # fi
+
+    #--------------------
+    # core dependencies
+    #--------------------
+
+    spack install -y openmpi %"$compiler"
+    # spack install -y openmpi cflags="$CFLAGS" cxxflags="$CXXFLAGS" fflags="$FFLAGS" %"$compiler"
+    # spack install -y mpich   cflags="$CFLAGS" cxxflags="$CXXFLAGS" fflags="$FFLAGS" %"$compiler"
+    # exit
 
 
-
-# without dependencies
-#~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-for pack in jemalloc gsl metis; do
-    spack install -y $pack %$compiler
-done;
-
-spack install -y hdf5 +cxx +fortran +threadsafe %$compiler
-spack install -y fftw@3.3.8 +openmp %$compiler
-spack install -y fftw@2.1.5 +openmp %$compiler
-# spack install -y grackle@3.2 ^hdf5 %$compiler #can't do grackle without MPI
+    #-----------------------
+    # individual packages
+    #-----------------------
 
 
 
-# with dependencies
-#~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # without dependencies
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# for dep in openmpi mpich; do
-for dep in openmpi; do
-    module load $dep
-
-    for pack in parmetis; do
-        spack install -y $pack ^$dep %$compiler
+    for pack in jemalloc gsl metis; do
+        spack install -y $pack %$compiler
     done;
 
-    # +openmp: install with openmp too
-    # ^$dep needs to be after +openmp, otherwise all other
-    # specifications are taken to be for the dependency
-    spack install -y hdf5 +cxx +fortran +threadsafe +mpi +java ^$dep %$compiler 
-    module load hdf5
-    spack install -y grackle@3.2 ^hdf5 ^$dep %$compiler
-done;
+    spack install -y fftw@3.3.8 +openmp %$compiler
+    spack install -y fftw@2.1.5 +openmp %$compiler
+    spack install --reuse -y hdf5@1.10.7 +cxx +fortran +threadsafe -mpi +java %$compiler
+    # spack install -y grackle@3.2 ^hdf5 %$compiler #can't do grackle without MPI
 
+
+
+    # with dependencies
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    # for dep in openmpi mpich; do
+    for dep in openmpi; do
+        module load $dep
+
+        # for pack in parmetis; do
+        #     spack install -y $pack ^$dep %$compiler
+        # done;
+
+        # +openmp: install with openmp too
+        # ^$dep needs to be after +openmp, otherwise all other
+        # specifications are taken to be for the dependency
+        # spack install --reuse -y hdf5@1.10.7 +cxx +fortran +threadsafe +mpi +java ^$dep %$compiler
+        # spack install --reuse -y hdf5@1.10.7 +cxx +fortran +threadsafe -mpi +java ^$dep %$compiler
+        module load hdf5
+        spack install --reuse -y grackle@3.2 ^hdf5 ^$dep %$compiler
+    done;
 
 done;
 
