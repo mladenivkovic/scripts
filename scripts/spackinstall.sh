ff3a459843ba872b789fd583ce1ad6007396ed9c
Mladen Ivkovic <mladen.ivkovic@hotmail.com>
2023-02-22 22:34:07 +0100

    updated spackinstall

--- scripts/spackinstall.sh
+++ scripts/spackinstall.sh
@@ -10,15 +10,16 @@
 # spack uninstall --all
 
 # my native compiler
-native="gcc@9.4.0"
+native="gcc@12.1.0"
 
 # First install lmod
 #----------------------
-# spack install -y lmod %"$native"
+spack install -y lmod %"$native"
 
 # make modules work
 # source ~/.bashrc_modules
 # exit
+
 # make sure you configured everything right before continuing with next steps
 # Note: lmod may have some issues if the 'Core' directory doesn't exist yet.
 # that directory gets created once a hierarchy exists, i.e. once you have
@@ -50,7 +51,8 @@ native="gcc@9.4.0"
 #------------------------------------------
 # for compiler in "$native" gcc@10.2.0 gcc@11.2.0 gcc@12.1.0; do
 # for compiler in "gcc@11.2.0"; do
-for compiler in "gcc@12.1.0"; do
+# for compiler in "gcc@12.1.0" "gcc@10.2.0"; do
+for compiler in "$native"; do
 
     module purge
     if [ "$compiler" == "$native" ]; then
@@ -80,7 +82,9 @@ for compiler in "gcc@12.1.0"; do
 
     spack install -y openmpi %"$compiler"
     # spack install -y mpich   cflags="$CFLAGS" cxxflags="$CXXFLAGS" fflags="$FFLAGS" %"$compiler"
-    # exit
+ 
+    spack module tcl rm # I only want lmod files, no need to show me modules twice
+    spack module lmod refresh --delete-tree -y # refresh lmod stuff
 
 
     #-----------------------
@@ -97,9 +101,9 @@ for compiler in "gcc@12.1.0"; do
     spack install -y fftw@3.3.8 +openmp %$compiler
     spack install -y fftw@2.1.5 +openmp %$compiler
     spack install --reuse -y hdf5@1.12.2 +cxx +fortran +threadsafe -mpi +java %$compiler
-    spack install --reuse -y hdf5-mladen@1.10.7 +cxx +fortran +threadsafe -mpi +java %$compiler
     # spack install -y grackle@3.2 ^hdf5 %$compiler #can't do grackle without MPI
-    spack install -y gdb@11.1 %$compiler
+    spack install -y gdb@12.1 %$compiler
+    spack install -y valgrind %$compiler
 
 
     # with dependencies
@@ -109,7 +113,7 @@ for compiler in "gcc@12.1.0"; do
     for dep in openmpi; do
         module load $dep
 
-        # packages without any further specifications
+        packages without any further specifications
         for pack in parmetis; do
             spack install -y $pack ^$dep %$compiler
         done;
@@ -118,12 +122,13 @@ for compiler in "gcc@12.1.0"; do
         # ^$dep needs to be after +openmp, otherwise all other
         # specifications are taken to be for the dependency
         spack install --reuse -y hdf5@1.12.2 +cxx +fortran +threadsafe +mpi +java ^$dep %$compiler
-        spack install --reuse -y hdf5-mladen@1.10.7 +cxx +fortran +threadsafe +mpi +java ^$dep %$compiler
-        module load hdf5@1.12.2
-        spack install --reuse -y grackle@3.2 ^hdf5@1.12.2 ^$dep %$compiler
+        # spack install --reuse -y hdf5-mladen@1.10.7 +cxx +fortran +threadsafe +mpi +java ^$dep %$compiler
+        module load hdf5/1.13.1
+        spack install --reuse -y grackle@3.2 ^hdf5@1.13.1 ^$dep %$compiler
         # note: grackle-float is a fake package I made.
         # If you don't want to use this, use +float variant.
-        spack install --reuse -y grackle-float@3.2 ^hdf5@1.12.2 ^$dep %$compiler
+        # spack install --reuse -y grackle-float@3.2 ^hdf5@1.13.1 ^$dep %$compiler
+        spack install --reuse -y grackle-float@3.2 ^hdf5@1.13.1 ^$dep %$compiler
         spack install --reuse -y sundials@5.1.0 ^$dep %$compiler
     done;
 
