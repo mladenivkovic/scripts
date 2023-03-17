8762bc59e2a4c582775bf72b4117918d0181b9e9
Mladen Ivkovic <mladen.ivkovic@hotmail.com>
2023-03-17 19:05:36 +0000

    another round of spack and apt updates

--- scripts/spackinstall.sh
+++ scripts/spackinstall.sh
@@ -11,12 +11,17 @@ MYHDF5="hdf5@1.12.2"
 
 # list of compilers you want to 
 declare -a compiler_list 
-compiler_list=($native_compiler)
-# compiler_list+=(gcc@11.3.0)
+# compiler_list=($native_compiler)
+# compiler_list+=(oneapi@2023.0.0)
+# compiler_list+=(clang@14.0.0)
+# compiler_list=(oneapi@2023.0.0)
+compiler_list=(clang@14.0.0)
+
 
 declare -a MPI_list 
 MPI_list=(openmpi)
-# MPI_list+=(mpich)
+MPI_list+=(mpich)
+# MPI_list=(mpich)
 
 
 
@@ -75,28 +80,54 @@ install_compilers() {
 
     # install compilers
     #----------------------
-    for compiler in ${compiler_list[@]}; do
+    # for compiler in ${compiler_list[@]}; do
+    #
+    #     if [ "$compiler" == "$native_compiler" ]; then
+    #         echo "skipping native compiler"
+    #         continue
+    #     fi
+    #
+    #     spack install -y "$compiler" %"$native_compiler"
+    #
+    #     # make sure spack knows about its compilers
+    #     # module purge
+    #     # compiler_module=$(spack_string_to_module_string $compiler)
+    #     # module load "$compiler_module"
+    # done
 
-        if [ "$compiler" == "$native_compiler" ]; then
-            echo "skipping native compiler"
-            continue
-        fi
+    spack install -y intel-oneapi-compilers@2023.0.0 %$native_compiler
+    spack install -y intel-oneapi-compilers-classic@2021.8.0 %$native_compiler
+    # spack install -y llvm +flang +ipo +omp_debug +omp_tsan +python  %$native_compiler
 
-        spack install -y "$compiler" %"$native_compiler"
+    # DONT INSTALL LLVM. USE SYTEM LLVM. It's set up in the packages file to use
+    # the external one. Just run a spack install on it anyway, so spack registers
+    # it as a spack package, and consequently modules are built.
+    # Also add gcc this way.
 
-        # make sure spack knows about its compilers
-        module purge
-        compiler_module=$(spack_string_to_module_string $compiler)
-        module load "$compiler_module"
-        spack compiler add
-        module purge
-    done
-    spack module tcl rm # I only want lmod files, no need to show me modules twice
+    spack module tcl rm -y # I only want lmod files, no need to show me modules twice
     spack module lmod refresh --delete-tree -y # refresh lmod stuff
     exit
     # make sure you configured everything right before continuing with next steps
 }
 
+
+
+
+load_compiler_module_only() {
+
+    compiler=$1
+
+    module purge
+    if [ "$compiler" == "$native_compiler" ]; then
+        echo "skipping native compiler module:" $native_compiler
+        compiler_module=""
+    else
+        # compiler_module=$(spack_string_to_module_string $compiler)
+        # TODO: hardcoded for now
+        compiler_module=intel-oneapi-compilers
+        module load "$compiler_module"
+    fi
+}
 
 
 #======================================================
@@ -108,17 +139,10 @@ install_packages() {
 
     for compiler in ${compiler_list[@]}; do
 
-        module purge
-        if [ "$compiler" == "$native_compiler" ]; then
-            echo "skipping native compiler module:" $native_compiler
-            compiler_module=""
-        else
-            compiler_module=$(spack_string_to_module_string $compiler)
-            module load "$compiler_module"
-        fi
-
+        load_compiler_module_only $compiler
 
         # clang needs extra flags
+        # TODO: is this still up to date? Shouldn't you add this to compilers.yml?
         # if [[ $compiler == "clang"* ]]; then
         #     echo got clang
         #     CFLAGS=-fPIC
@@ -134,65 +158,101 @@ install_packages() {
         # core dependencies
         #--------------------
 
-        # spack install -y openmpi %"$compiler"
-        # # spack install -y mpich   cflags="$CFLAGS" cxxflags="$CXXFLAGS" fflags="$FFLAGS" %"$compiler"
-        #
-        # spack module tcl rm -y # I only want lmod files, no need to show me modules twice
-        # spack module lmod refresh --delete-tree -y # refresh lmod stuff
-        #
-        #
-        # #-----------------------
-        # # individual packages
-        # #-----------------------
+        additional_specs=""
+        if [[ "$compiler" == "clang"* ]]; then
+            additional_specs="~fortran"
+        fi
 
-        # # without dependencies
-        # #~~~~~~~~~~~~~~~~~~~~~~~~~~
-        #
-        # # cblas: doesn't compile...
-        # for pack in jemalloc gsl metis intel-tbb openblas; do
-        #     spack install -y $pack %$compiler
-        # done;
-        #
-        # spack install -y fftw@3.3.8 +openmp %$compiler
-        # spack install -y fftw@2.1.5 +openmp %$compiler
-        # spack install --reuse -y "$MYHDF5" +cxx +fortran +threadsafe -mpi +java %$compiler
-        # # spack install -y grackle@3.2 ^"$MYHDF5" %$compiler #can't do grackle without MPI
-        #
-        # spack install -y gdb@12.1
-        # spack install -y valgrind
-        # # spack install elfutils@0.188 -nls %gcc@12.1.0 # temporary manual fix for elfutils, needed by gdb
-        # # spack install -y gdb@13.1 ^elfutils@0.188 -nls %$compiler
-        # # spack install -y valgrind ^elfutils@0.188 -nls %$compiler
-        #
- 
+        for mpi_vendor in ${MPI_list[@]}; do
+            spack install -y $mpi_vendor $additional_specs %"$compiler"
+        done
+        # # spack install -y mpich   cflags="$CFLAGS" cxxflags="$CXXFLAGS" fflags="$FFLAGS" %"$compiler"
+
+        spack module tcl rm -y # I only want lmod files, no need to show me modules twice
+        spack module lmod refresh --delete-tree -y # refresh lmod stuff
+
+
+        #-----------------------
+        # individual packages
+        #-----------------------
+
+        # without dependencies
+        #~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        # packages without fortran variants go here
+        for pack in jemalloc gsl metis intel-tbb; do
+            spack install -y $pack %$compiler
+        done;
+
+        # packages with fortran variants go here
+        for pack in openblas; do
+            spack install -y $pack $additional_specs %$compiler
+        done;
+
+
+        spack install -y fftw@3.3.8 +openmp ~mpi %$compiler
+        spack install -y fftw@2.1.5 +openmp ~mpi %$compiler
+        if [[ "$compiler" == "clang"* ]]; then
+            spack install -y "$MYHDF5" +cxx ~fortran +threadsafe ~mpi +java %$compiler
+        else
+            spack install -y "$MYHDF5" +cxx +fortran +threadsafe ~mpi +java %$compiler
+        fi
+        # spack install -y grackle@3.2 ^"$MYHDF5" %$compiler #can't do grackle without MPI
+
+        # gdb doesn't work at the moment...
+        # spack install -y gdb
+        # valgrind has no fortran variant
+        spack install valgrind ~mpi %$compiler
+
         # with dependencies
         #~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-        # for dep in openmpi mpich; do
         for dep in ${MPI_list[@]}; do
+
+            if [[ "$compiler" == "clang"* ]]; then
+                if [[ "$dep" == "openmpi"* ]]; then
+                    echo skipping clang + openmpi due to no fortran compiler
+                    continue
+                fi
+            fi
+
+            load_compiler_module_only $compiler
             module load $dep
 
             # packages without any further specifications
-            for pack in parmetis; do
-                spack install -y $pack ^$dep %$compiler
+            for pack in parmetis valgrind; do
+                # neither valgrind nor parmetis have fortran variant
+                spack install -y $pack ^$dep $additional_specs %$compiler
             done;
 
             # +openmp: install with openmp too
             # ^$dep needs to be after +openmp, otherwise all other
             # specifications are taken to be for the dependency
-            spack install --reuse -y "$MYHDF5" +cxx +fortran +threadsafe +mpi +java ^$dep %$compiler
+            if [[ "$compiler" == "clang"* ]]; then
+                spack install -y "$MYHDF5" +cxx ~fortran +threadsafe +mpi +java ^$dep %$compiler
+            else
+                spack install -y "$MYHDF5" +cxx +fortran +threadsafe +mpi +java ^$dep %$compiler
+            fi
             # spack install --reuse -y hdf5-mladen@1.10.7 +cxx +fortran +threadsafe +mpi +java ^$dep %$compiler
+
             spack module tcl rm -y # I only want lmod files, no need to show me modules twice
             spack module lmod refresh --delete-tree -y # refresh lmod stuff
+            module purge
+
+            module load $compiler_module
             module load hdf5/${MYHDF5#hdf5@}
 
-            
-            spack install --reuse -y grackle@3.2 ^$MYHDF5 ^$dep %$compiler
+            spack install -y fftw@3.3.8 +openmp +mpi ^$dep $additional_specs %$compiler
+            spack install -y fftw@2.1.5 +openmp +mpi ^$dep $additional_specs %$compiler
+
+
+            spack install --reuse -y grackle@3.2 ^$MYHDF5 ^$dep $additional_specs %$compiler
             # note: grackle-float is a fake package I made.
             # If you don't want to use this, use +float variant.
             # spack install --reuse -y grackle-float@3.2 ^hdf5@1.13.1 ^$dep %$compiler
-            spack install --reuse -y grackle-float@3.2 ^$MYHDF5 ^$dep %$compiler
-            spack install --reuse -y sundials@5.1.0 ^$dep %$compiler
+            # spack install --reuse -y grackle-float@3.2 ^$MYHDF5 ^$dep %$compiler
+            spack install --reuse -y sundials@5.1.0 $additional_specs ^$dep $additional_specs %$compiler
+
         done;
 
     done;
@@ -208,7 +268,9 @@ install_packages() {
 # MAIN COURSE
 #======================================================
 
+# first find external, already existing packages
+# spack external find
 # uninstall_everything
 # install_modules
-# # install_compilers
+# install_compilers # remember to manually add system compilers by making them unbuildable in packages.yaml, but installing them with spack anyway.
 install_packages
