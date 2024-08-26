363aa4d017e4a5f5d779c65c7bf4e16dfc7956b7
Mladen Ivkovic <mladen.ivkovic@hotmail.com>
2024-08-26 12:56:45 +0100

    added option to overwrite stuff on HD

--- scripts/backups/syncHD.sh
+++ scripts/backups/syncHD.sh
@@ -9,12 +9,43 @@
 ##############################################
 
 
-#---------------------------------------------------------------------------------
-#
-# usage:
-#   $syncHD.sh          backs up all directories hardcoded in this script
-#
-#---------------------------------------------------------------------------------
+errmsg="
+Sync up all directories hardcoded in this script onto hard drives,
+whose paths are also hardcoded in this script.
+
+usage:
+    $ syncHD.sh               backs up all directories hardcoded in this script
+    $ syncHD.sh  -h           show this message and quit
+                 --help
+    $ syncHD.sh  -f           Don't syncronize to and from the HD backups, but
+                 --force      forcibly overwrite what is on the harddrives with
+                 -o           the current state.
+                 -ow
+                 --overwrite
+"
+#-----------------------------------------------------------------------
+# First, let's read the cmdline args.
+#-----------------------------------------------------------------------
+
+OVERWRITE="no"
+while [[ $# > 0 ]]; do
+    arg="$1"
+
+    case $arg in
+        -h | --help)
+            echo "$errmsg"
+            exit 0
+        ;;
+
+        -f | --force | -o | -ow | --overwrite)
+            OVERWRITE="yes"
+            echo "Will overwrite backup instead of syncing"
+        ;;
+
+    esac
+
+    shift
+done
 
 
 
@@ -24,24 +55,32 @@
 # Choices are hardcoded in here.
 # TODO: Make sure this fails and exits if no HDPATH is found.
 # Otherwise, the subsequent rsync calls are going to do stupid things.
+#
+# @Mladen: Note that this sync doesn't require encrypted drives.
 #------------------------------------------------------------------------
 find_HD_path(){
-    HDPATH='/media/mivkov/WD free/'
+
+    HOMEDIR_BASENAME=`basename $HOME`
+    HDPATH="/media/$HOMEDIR_BASENAME/WD free/"
+    # This is the "home" HD. Include private/personal documents in the sync.
+    INCLUDE_PRIVATE="yes"
 
     if [ ! -d "$HDPATH" ]; then
-        echo "Din't find target dir" $DESTDIR "trying second option"
-        HDPATH='/media/mivkov/archive/'
+        echo "Din't find target dir '"$HDPATH"', trying second option"
+        HDPATH="/media/$HOMEDIR_BASENAME/archive/"
         if [ ! -d "$HDPATH" ]; then
-            echo "Din't find target dir" $DESTDIR 
+            echo "Din't find target dir" $HDPATH
             exit 1
         fi
+        # This is the "work" HD. Don't include private/personal documents in the sync.
+        INCLUDE_PRIVATE="no"
     fi
 }
 
 
 
 #------------------------------------------------------------------------
-# Synchronize a single directory (recursively) back and forth between 
+# Synchronize a single directory (recursively) back and forth between
 # the HD and local machine.
 #
 # Usage:
@@ -93,13 +132,13 @@ sync_dir() {
 
     # make sure the dirs don't have trailing slashes. This modifies
     # rsync behaviour.
-    # RUn it twice in case someone has stupid ideas.
-    
+    # Run it twice in case someone has stupid ideas.
+
     HDDIR=${HDDIR%/}
     HDDIR=${HDDIR%/}
     LOCALDIR=${LOCALDIR%/}
     LOCALDIR=${LOCALDIR%/}
-    
+
     # Check that directories in fact exist.
     if [ ! -d "$HDDIR" ]; then
         echo "Target directory on HD not found. Target is '"$HDDIR"', HDPATH is '"$HDPATH"'"
@@ -122,7 +161,7 @@ sync_dir() {
         newarg_local_full=$1
 
         # check full proper paths
-        # WARNING: Don't actually use full paths. 
+        # WARNING: Don't actually use full paths.
         if [[ "$newarg_local_full" = /* ]]; then
             : # this should be a full path. `:` means do nothing.
         else
@@ -144,7 +183,7 @@ sync_dir() {
             echo "WARNING: Directory to exclude doesn't exist on HD. Passed argument was '"$1"', full path is '"$newarg_HD_full"'"
         fi
 
-     
+
         excludestr_rsync_local="$excludestr_rsync_local ""--exclude=$newarg_local/**"" --exclude=$newarg_local "
         excludestr_rsync_HD="$excludestr_rsync_HD ""--exclude=$newarg_HD/**"" --exclude=$newarg_HD "
         shift
@@ -160,6 +199,30 @@ sync_dir() {
                         --stats \
                         --update \
                         --recursive \
+                        "
+
+    RSYNC_CMD_DELETE_FIRST="rsync   --archive \
+                                    --verbose \
+                                    --human-readable \
+                                    --progress \
+                                    --stats \
+                                    --update \
+                                    --recursive \
+                                    --delete \
+                                    "
+
+    RSYNC_CMD_DELETE_FIRST_DRY_RUN="rsync   --archive \
+                                            --human-readable \
+                                            --recursive \
+                                            --verbose \
+                                            --stats \
+                                            --update \
+                                            --delete \
+                                            --dry-run \
+                                            "
+
+
+    RSYNC_CMD_DEFAULT_EXCLUDES="
                         --exclude=**/*tmp*/ \
                         --exclude=**/*cache*/ \
                         --exclude=**/*Cache*/ \
@@ -169,32 +232,56 @@ sync_dir() {
                         --exclude=**/*trash*/ \
                         --exclude=**/.gvfs/ "
 
+    RSYNC_CMD=${RSYNC_CMD}${RSYNC_CMD_DEFAULT_EXCLUDES}
+    RSYNC_CMD_DELETE_FIRST=${RSYNC_CMD_DELETE_FIRST}${RSYNC_CMD_DEFAULT_EXCLUDES}
 
-    # Sync LOCAL to HD
-    echo "==================================================================================="
-    echo "TRANSFERING" $LOCALDIR " --> "  $HDDIR
-    echo "==================================================================================="
-    $RSYNC_CMD $excludestr_rsync_local --log-file=logs/rsync-L2HD-"$DATE"".log" "$LOCALDIR"/ "$HDDIR"
-    # Sync HD to LOCAL
-    echo "==================================================================================="
-    echo "TRANSFERING" $HDDIR " --> "  $LOCALDIR
-    echo "==================================================================================="
-    $RSYNC_CMD $excludestr_rsync_HD --log-file=logs/rsync-HD2L-"$DATE"".log" "$HDDIR"/ "$LOCALDIR"
+
+    if [ ${OVERWRITE} == "yes" ]; then
+        echo "==================================================================================="
+        echo "OVERWRITING" $LOCALDIR " --> "  $HDDIR
+        echo "==================================================================================="
+        $RSYNC_CMD_DELETE_FIRST_DRY_RUN $excludestr_rsync_local "$LOCALDIR"/ "$HDDIR"
+        while true; do
+            read -p "That was a dry run. Do you wish to continue? (y/n) " yn
+            case $yn in
+                [Yy]* ) break;;
+                [Nn]* ) echo "exiting."; exit;;
+                * ) echo "Please answer yes or no.";;
+            esac
+        done
+        $RSYNC_CMD_DELETE_FIRST $excludestr_rsync_local --log-file=logs/rsync-L2HD-overwrite-"$DATE"".log" "$LOCALDIR"/ "$HDDIR"
+
+        # When overwriting, we only do L->HD.
+
+    else
+        echo "SYNCING"
+        # Sync LOCAL to HD
+        echo "==================================================================================="
+        echo "TRANSFERING" $LOCALDIR " --> "  $HDDIR
+        echo "==================================================================================="
+        $RSYNC_CMD $excludestr_rsync_local --log-file=logs/rsync-L2HD-"$DATE"".log" "$LOCALDIR"/ "$HDDIR"
+        # Sync HD to LOCAL
+        echo "==================================================================================="
+        echo "TRANSFERING" $HDDIR " --> "  $LOCALDIR
+        echo "==================================================================================="
+        $RSYNC_CMD $excludestr_rsync_HD --log-file=logs/rsync-HD2L-"$DATE"".log" "$HDDIR"/ "$LOCALDIR"
+    fi
 
 
 }
 
 
 
-
-
-
-
 find_HD_path
+# note: everything past the 2nd arg is to be excluded
 sync_dir $HOME/Work Work
-sync_dir $HOME/Pictures/Memories Pictures/Memories videos childhood Pre-2018 2018 2019 2020 2021 2022
 sync_dir $HOME/Documents/Bewerbungen Documents/Bewerbungen
-sync_dir $HOME/Documents/Wichtige_Dokumente Documents/Wichtige_Dokumente
+sync_dir $HOME/Zotero Zotero
+
+if [ ${INCLUDE_PRIVATE} = "yes" ]; then
+    sync_dir $HOME/Pictures/Memories Pictures/Memories videos childhood Pre-2018 2018 2019 2020 2021 2022
+    sync_dir $HOME/Documents/Wichtige_Dokumente Documents/Wichtige_Dokumente
+fi
 
 
 echo DONE.
