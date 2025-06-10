43a9ba1861f3a2bb54b30e8df1caf04c7453dcb3
Mladen Ivkovic <mladen.ivkovic@hotmail.com>
2025-06-10 22:35:07 +0100

    added allclean script

---
+++ allclean.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Search for generated/compiled files and clean them up.
+
+
+CWD=`pwd`
+
+texclean() {
+  FILE=$1
+  DIR=`dirname "$FILE"`
+  cd "$DIR"
+  echo "=== TEX CLEAN $DIR"
+  rm -fv *.aux *.bbl *.log *.out *.gz *.toc *.blg *.lot *.lof *.run.xml *.snm *.nav *-blx.bib
+  cd "$CWD"
+}
+
+# need to export the function so that sub-shells will recognize it.
+# can't use `find` to run it otherwise.
+export -f texclean
+
+# -exec needs to be terminated with an escaped \; otherwise the shell interprets it as line end.
+find . -name "*.tex" -exec bash -c 'texclean "$0"' {} \;
+
+
+
+# Check for makefiles
+for MKFILE in `find . -name "Makefile"`; do
+  MK_DIR=`dirname "$MKFILE"`
+  cd "$MK_DIR"
+
+  # Check that we don't have tex files in this directory. If so, running a
+  # make clean could delete the pdf, which is not what we want.
+  if [[ `find -maxdepth 1 -name "*.tex"` ]]; then
+    cd "$CWD"
+    continue
+  fi
+
+  echo "=== MAKE CLEAN $MK_DIR"
+  make distclean || make clean
+
+  cd "$CWD"
+done
+
+
+
