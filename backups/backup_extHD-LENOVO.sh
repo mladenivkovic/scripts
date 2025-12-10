708388b61bb42d0811f64c6cc75472c20b3ed035
Mladen Ivkovic <mivkov@protonmail.com>
2025-12-10 17:33:56 +0000

    added minimal backup to lenovo

--- backups/backup_extHD-LENOVO.sh
+++ backups/backup_extHD-LENOVO.sh
@@ -7,143 +7,225 @@
 ##############################################
 
 
-#---------------------------------------------------------------------------------
-#
-# usage:
-#   $backup_extHD.sh          backs up all directories hardcoded in this script
-#   $backup_extHD.sh <dir>    backs up only given directory, if it is part of
-#                             hardcoded directories
-#
-#---------------------------------------------------------------------------------
+ERRMSG='
+backup_extHD-HP.sh - back up your data to external HD.
+                     Backs up all directories hardcoded in this script.
+
+usage:
+  backup_extHD-HP.sh [dir] [-h] [-m]
+
+  [dir]         if provided, backs up only given directory, if it is part of
+                hardcoded directories
+  -h, --help    show this message and exit.
+  -m, --minimal only do minimal backup (only very selected, hardcoded dirs)
+'
 
 
 
-ROOT_BACKUP_DIR=$HOME                        # Root dir to backup
-DATE=`date +%F_%Hh%M`                        # current time
-BACKUP_DIR=/run/media/mivkov/BACKUP_LENOVO/  # where to store the backup
+ROOT_BACKUP_SRC_DIR=$HOME                      # Root dir to backup
+DATE=`date +%F_%Hh%M`                          # current time
+BACKUP_DEST_DIR=/home/mivkov/Encfs/BACKUP_HP/  # where to store the backup
+HOMEDIR_BASENAME=`basename $HOME`
 
-if [ ! -d "$BACKUP_DIR" ]; then
-  echo "Din't find target dir" $BACKUP_DIR
+
+# Handle cmdline args.
+# --------------------
+
+MINIMAL="false"
+BACKUP_SRC_DIR="#none"
+
+while [[ $# > 0 ]]; do
+  ARG="$1"
+
+  case $ARG in
+    -h | --help)
+      echo "$ERRMSG"
+      exit
+    ;;
+
+    -m | --minimal)
+      MINIMAL="true"
+    ;;
+
+    *)
+      BACKUP_SRC_DIR=`realpath $ARG`
+      if [ ! -d "$BACKUP_SRC_DIR" ]; then
+        echo "Error: Didn't find directory " $BACKUP_SRC_DIR
+      fi
+    ;;
+
+  esac
+
+  shift
+done
+
+
+if [ ! -d "$BACKUP_DEST_DIR"/$HOMEDIR_BASENAME ]; then
+  echo "Din't find target dir '"$BACKUP_DEST_DIR/$HOMEDIR_BASENAME"', trying second option"
+  # echo "Did you remember to mount the encrypted drives?"
   exit 1
 fi
 
+echo Writing backup to "$BACKUP_DEST_DIR"
 
-EXCLUDES="" # Define parent directories that are to be excluded here
-EXCLUDES="$EXCLUDES Audiobooks"
-EXCLUDES="$EXCLUDES Dropbox"
-EXCLUDES="$EXCLUDES Podcasts"
-EXCLUDES="$EXCLUDES Downloads"
-EXCLUDES="$EXCLUDES Encfs"
-EXCLUDES="$EXCLUDES texmf"
-EXCLUDES="$EXCLUDES .dbus"
+
+EXCLUDEDIRS="" # Define parent directories that are to be excluded here
+# EXCLUDEDIRS="$EXCLUDEDIRS Audiobooks"
+# EXCLUDEDIRS="$EXCLUDEDIRS Downloads"
+# EXCLUDEDIRS="$EXCLUDEDIRS Dropbox"
+# EXCLUDEDIRS="$EXCLUDEDIRS dwhelper"
+EXCLUDEDIRS="$EXCLUDEDIRS Encfs"
+EXCLUDEDIRS="$EXCLUDEDIRS google-drive"
+# EXCLUDEDIRS="$EXCLUDEDIRS Music"
+# EXCLUDEDIRS="$EXCLUDEDIRS Podcasts"
+# EXCLUDEDIRS="$EXCLUDEDIRS Steam"
+# EXCLUDEDIRS="$EXCLUDEDIRS Templates"
+# EXCLUDEDIRS="$EXCLUDEDIRS texmf"
+# EXCLUDEDIRS="$EXCLUDEDIRS Videos"
+EXCLUDEDIRS="$EXCLUDEDIRS .dbus"
+
+EXCLUDEFILES="" # Define file patterns that are to be excluded here
+EXCLUDEFILES="$EXCLUDEFILES **/Peano/**/celldata/**"
+EXCLUDEFILES="$EXCLUDEFILES **/Peano/**/globaldata/**"
+EXCLUDEFILES="$EXCLUDEFILES **/Peano/**/observers/**"
+EXCLUDEFILES="$EXCLUDEFILES **/Peano/**/repositories/**"
+EXCLUDEFILES="$EXCLUDEFILES **/Peano/**/vertexdata/**"
+EXCLUDEFILES="$EXCLUDEFILES **/Peano/**/*.o"
+EXCLUDEFILES="$EXCLUDEFILES **/Peano/**/*.a"
+EXCLUDEFILES="$EXCLUDEFILES **/Peano/**/*.Po"
+EXCLUDEFILES="$EXCLUDEFILES **/Peano/doxygen-html/**"
+EXCLUDEFILES="$EXCLUDEFILES **/swiftsim/**/*.o"
+EXCLUDEFILES="$EXCLUDEFILES **/swiftsim/**/*.lo"
+EXCLUDEFILES="$EXCLUDEFILES **/swiftsim/**/*.Plo"
 
 # generate exclusion string for rsync
 excludestr_rsync=""
-for EX in $EXCLUDES; do
+for EX in $EXCLUDEDIRS; do
   excludestr_rsync="$excludestr_rsync ""--exclude=$EX/**"" --exclude=$EX "
 done
-
-# echo "|"$EXCLUDES"|"
-# echo
-# echo $excludestr_rsync
-# exit
+for EX in $EXCLUDEFILES; do
+  excludestr_rsync="$excludestr_rsync --exclude=$EX "
+done
 
 
+# indended usage: $rsync_cmd "$BACKUP_SRC_DIR" "$BACKUP_DEST_DIR"
+rsync_cmd=rsync
+rsync_cmd+=" --archive --verbose --human-readable --progress --stats"
+rsync_cmd+=" --update --recursive --delete --exclude=**/*tmp*/ --exclude=**/*cache*/"
+rsync_cmd+=" --exclude=**/*Cache*/ --exclude=**~ --exclude=/mnt/*/** --exclude=/media/*/**"
+rsync_cmd+=" --exclude=**/lost+found*/ --exclude=**/*Trash*/ --exclude=**/*trash*/"
+rsync_cmd+=" --exclude=**/.gvfs/ --log-file=rsync-backup-HP-""$DATE"".log"
+rsync_cmd+=" ""$excludestr_rsync"
+  # covered by --archive
+  #   --recursive \
+  #   --times \
+  #   --devices \
+  #   --specials \
+  #   --links \
+  #   --perms \
 
-# get and prepare cmdline args
-if [ $# = 0 ]; then
-  echo "Doing full backup."
-  do_full=true
-  DIR_TO_BACKUP="$ROOT_BACKUP_DIR"
-elif [ $# == 1 ]; then
-  do_full=false
-  dir_given=$1
-  DIR_TO_BACKUP=`realpath $dir_given`
-  if [ ! -d $DIR_TO_BACKUP ]; then
-    echo "Didn't find directory you've provided: '"$dir_given"'"
+  # --delete-excluded \
+
+
+# if we're doing only a single dir backup:
+if [[ "$BACKUP_SRC_DIR" != "#none" ]]; then
+
+  # first check that it's within ROOT_BACKUP_SRC_DIR
+  if [[ ! "$BACKUP_SRC_DIR" = "$ROOT_BACKUP_SRC_DIR"* ]]; then
+    echo "$BACKUP_SRC_DIR" is not in the root backup directory $ROOT_BACKUP_SRC_DIR
     exit 1
-  else
-    echo "Doing partial backup of" "$dir_given"
-  fi
-else
-  echo "Too many cmdline args. I only accept 1 (specific dir to backup) or none (do full backup)"
-  exit 1
-fi
-
-
-# if partial backup:
-if [[ "$do_full" = false ]]; then
-
-  # first check that it's within ROOT_BACKUP_DIR
-  if [[ ! "$DIR_TO_BACKUP" = "$ROOT_BACKUP_DIR"* ]]; then
-    echo "$dir_given" is not in the root backup directory $ROOT_BACKUP_DIR
-    exit 1
-  else
-    echo "Continuing partial backup, is in root dir"
   fi
 
   # then check that it's not in an excluded dir
   for EX in $EXCLUDES; do
-    if [[ "$DIR_TO_BACKUP" = "$EX"* ]]; then
-      echo "$dir_given" is in an excluded backup directory: $EX
+    if [[ "$BACKUP_SRC_DIR" = "$EX"* ]]; then
+      echo "$BACKUP_SRC_DIR" is in an excluded backup directory: $EX
       exit 1
     fi
   done
-  echo "Continuing partial backup, not in excludes"
 
-
-  # now add the additional path of the directory to BACKUP_DIR
-  parent_root_dir="$(dirname $ROOT_BACKUP_DIR)"
-  parent_dir_to_backup="$(dirname $DIR_TO_BACKUP)"
-  BACKUP_DIR="$BACKUP_DIR"/"${parent_dir_to_backup#$parent_root_dir}"
-  mkdir -p "$BACKUP_DIR"
+  # now add the additional path of the directory to BACKUP_DEST_DIR
+  parent_root_dir="$(dirname $ROOT_BACKUP_SRC_DIR)"
+  parent_backup_src_dir="$(dirname $BACKUP_SRC_DIR)"
+  BACKUP_DEST_DIR="$BACKUP_DEST_DIR"/"${parent_backup_src_dir#$parent_root_dir}"
+  mkdir -p "$BACKUP_DEST_DIR"
   # example:
   #   initially:
-  #   BACKUP_DIR = /media/mivkov/backup
-  #   DIR_TO_BACKUP = /home/mivkov/xkcd/oglaf/ToG
+  #   BACKUP_DEST_DIR = /media/mivkov/backup
+  #   BACKUP_SRC_DIR = /home/mivkov/xkcd/ksbd/ToG
   #
   #   change into:
   #   parent_root_dir = /home
-  #   parent_dir_to_backup = /home/mivkov/xkcd/oglaf
-  #   BACKUP_DIR += /mivkov/xkcd
+  #   parent_dir_to_backup = /home/mivkov/xkcd/ksbd
+  #   BACKUP_DEST_DIR += /mivkov/xkcd
+
+  echo "---Backing up single dir '$BACKUP_SRC_DIR'---"
+  $rsync_cmd "$BACKUP_SRC_DIR" "$BACKUP_DEST_DIR"
+  echo "---Single dir backup ended---"
+
+  exit
+fi
+
+
+MINIMAL_ROOT_SOURCES=" $HOME/Documents "
+MINIMAL_ROOT_SOURCES+=" $HOME/Durham "
+MINIMAL_ROOT_SOURCES+=" $HOME/EPFL "
+MINIMAL_ROOT_SOURCES+=" $HOME/Pictures "
+# MINIMAL_ROOT_SOURCES+=" $HOME/Work "
+
+
+# if we're doing a "minimal" backup:
+if [[ "$MINIMAL" = "true" ]]; then
+
+  echo "---Starting minimal backup---"
+
+  for DIR in $MINIMAL_SOURCES; do
+
+    BACKUP_SRC_DIR=`realpath $DIR`
+
+    # first check that it's within ROOT_BACKUP_SRC_DIR
+    if [[ ! "$DIR" = "$ROOT_BACKUP_SRC_DIR"* ]]; then
+      echo "$BACKUP_SRC_DIR" is not in the root backup directory $ROOT_BACKUP_SRC_DIR
+      exit 1
+    fi
+
+    # then check that it's not in an excluded dir
+    for EX in $EXCLUDES; do
+      if [[ "$BACKUP_SRC_DIR" = "$EX"* ]]; then
+        echo "$BACKUP_SRC_DIR" is in an excluded backup directory: $EX
+        exit 1
+      fi
+    done
+
+    # now add the additional path of the directory to BACKUP_DEST_DIR
+    parent_root_dir="$(dirname $ROOT_BACKUP_SRC_DIR)"
+    parent_backup_src_dir="$(dirname $BACKUP_SRC_DIR)"
+    BACKUP_DEST_DIR="$BACKUP_DEST_DIR"/"${parent_backup_src_dir#$parent_root_dir}"
+    mkdir -p "$BACKUP_DEST_DIR"
+    # example:
+    #   initially:
+    #   BACKUP_DEST_DIR = /media/mivkov/backup
+    #   BACKUP_SRC_DIR = /home/mivkov/xkcd/ksbd/ToG
+    #
+    #   change into:
+    #   parent_root_dir = /home
+    #   parent_dir_to_backup = /home/mivkov/xkcd/ksbd
+    #   BACKUP_DEST_DIR += /mivkov/xkcd
+
+    echo "---Minimal backup: backing '$BACKUP_SRC_DIR'---"
+    $rsync_cmd "$BACKUP_SRC_DIR" "$BACKUP_DEST_DIR" --log-file=rsync-backup-HP-"$DATE"-"$DIR".log
+
+  done
+  echo "---Minimal backup ended---"
+  exit 0
 
 fi
 
 
+# Doing full backup, then
+BACKUP_SRC_DIR="$ROOT_BACKUP_SRC_DIR"
 
-echo "---Backup started---"
-
-
-# =====================================
-# Private files
-# =====================================
-
-echo "$DIR_TO_BACKUP"
-
-rsync   --archive \
-        --verbose \
-        --human-readable \
-        --progress \
-        --stats \
-        --update \
-        --recursive \
-        --delete \
-        --exclude=/mnt/*/** \
-        --exclude=/media/*/** \
-        --exclude=**/lost+found*/ \
-        --exclude=**/.gvfs/ \
-        $excludestr_rsync \
-        --log-file=rsync-backup-LENOVO-FULL-"$DATE"".log" \
-        "$DIR_TO_BACKUP" "$BACKUP_DIR"
-        # covered by --archive
-        #   --recursive \
-        #   --times \
-        #   --devices \
-        #   --specials \
-        #   --links \
-        #   --perms \
-
-        # --delete-excluded \
-
-echo "---Backup ended---"
+echo "---Starting full backup---"
+$rsync_cmd "$BACKUP_SRC_DIR" "$BACKUP_DEST_DIR"
+echo "---Full backup ended---"
 exit 0
